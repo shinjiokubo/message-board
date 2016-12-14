a007184580d9d4941c035c24928f323a8ef42f5b
shinjiookubo <zyuriroba999@yahoo.co.jp>
2016-12-14 08:12:49 +0000

    implementation of the calidation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,20 @@
 class MessagesController < ApplicationController
-  def index
-    @message = Message.new
+    def index
+      @message = Message.new
         # Messageを全て取得する。
-    @messages = Message.all
+      @messages = Message.all
   end
 
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      #メッセージが保存できなかった時
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
   end
 
   private
