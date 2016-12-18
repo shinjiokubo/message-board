4539e13e569e3c9ed7704717a358dde6890cfd3d
shinjiookubo <zyuriroba999@yahoo.co.jp>
2016-12-18 09:50:16 +0000

    amend MB

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,6 @@ class Message < ActiveRecord::Base
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
   # 年齢（age)に0から150以下の数字だけ受け付ける
-  validates :age , numericality: true , inclusion: {in: 0..150 } 
+  validates :age , numericality: {only_integer:true , greater_than_or_equal_to: 0 ,less_than_or_equal_to: 150}
 end
 
